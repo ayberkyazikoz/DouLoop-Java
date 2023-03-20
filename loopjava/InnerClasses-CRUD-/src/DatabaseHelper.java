6280409a0f9b038542db905bcfadaa4a946e0897
Ayberk Yazıkoz <93318940+ayberkyazikoz@users.noreply.github.com>
2023-03-20 13:18:54 +0300

    staticDemo ve InnerClasses eklendi

---
+++ loopjava/InnerClasses-CRUD-/src/DatabaseHelper.java
@@ -0,0 +1,40 @@
+public class DatabaseHelper {
+    public static class Crud
+    {
+        public static void Create()
+        {
+            System.out.println("Create.");
+        }
+        public static void Read()
+        {
+            System.out.println("Read.");
+        }
+        public static void Update()
+        {
+            System.out.println("Update.");
+        }
+        public static void Delete()
+        {
+            System.out.println("Delete.");
+        }
+    }
+    public static class Connection
+    {
+        public static void CreateConnection()
+        {
+            System.out.println("Create Con.");
+        }
+        public static void ReadConnection()
+        {
+            System.out.println("Read Con.");
+        }
+        public static void UpdateConnection()
+        {
+            System.out.println("Update Con.");
+        }
+        public static void DeleteConnection()
+        {
+            System.out.println("Update Con.");
+        }
+    }
+}
